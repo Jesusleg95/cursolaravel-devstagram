73e977039308c2b95cbc363654ee830dd78d8372
Jesus Lechuga <jesusleg95@outlook.com>
2026-01-12 13:34:09 -0700

    Mostrar Comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ComentarioController extends Controller
 {
-    public function store(){
+    use ValidatesRequests;
+
+    public function store(Request $request, User $user, Post $post){
         //Validar
+        $this->validate($request, [
+            'comentario' => 'required|max:255'
+        ]);
+
+        //Almacenar el resultado
+        Comentario::create([
+            'user_id' => Auth::user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
+
+        //Imprimir mensaje
+        return back()->with('mensaje', 'Comentario Realizado Correctamente');
         
     }
 }
--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'comentario'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
